4cd6718ce307de2c6cdedd1123c7943173be270c
a.b.butorin <a.b.butorin@tinkoff.ru>
2025-02-17 09:40:02 +0300

    Basic JUnit Annotations

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,5 +1,12 @@
 class Calculator {
     val addition = { a: Double, b: Double -> a + b }
+    val subtraction = { a: Double, b: Double -> a - b }
+    val multiplication = { a: Double, b: Double -> a * b }
+    val division = { a: Double, b: Double ->
+        if (b == 0.0) throw UnsupportedOperationException("Division by zero")
+        else a / b
+    }
+
 
     fun calculate(
         lambda: ((Double, Double) -> Double),
--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -1,12 +1,65 @@
+import org.junit.After
+import org.junit.AfterClass
 import org.junit.Assert.*
+import org.junit.Assume.assumeTrue
+import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
+@RunWith(JUnit4::class)
 class CalculatorTest{
 
+    lateinit var calculator : Calculator
+
+    @Before
+    fun setUp() {
+        calculator = Calculator()
+        println("In @Before setUp()")
+    }
+
     @Test
-    fun calculatorTest() {
+    fun calculatorTest1() {
         with(Calculator()) {
+            println("In @Test calculatorTest1()")
             assertEquals("Ой", 5.0, calculate(addition, 2.0, 3.0), 0.0)
+        }
+    }
+
+    @Test(expected = UnsupportedOperationException::class)
+    fun calculatorTest2() {
+        with(Calculator()) {
+            println("In @Test calculatorTest2()")
+            assertEquals("Ой", 4.0, calculate(division, 2.0, 0.0), 0.0)
+        }
+    }
+
+    @Test
+    fun calculatorTest3() {
+        with(Calculator()) {
+            assumeTrue(false)
+            println("In @Test calculatorTest3()")
+            assertEquals("Ой", 3.0, calculate(addition, 1.0, 2.0), 0.0)
+        }
+    }
+
+    @After
+    fun tearDown() {
+        println("In @After tearDown()")
+    }
+
+    companion object {
+        @JvmStatic
+        @BeforeClass
+        fun beforeClassMethod(): Unit {
+            println("In @BeforeClass beforeClassMethod()")
+        }
+
+        @JvmStatic
+        @AfterClass
+        fun afterClassMethod(): Unit {
+            println("In @AfterClass afterClassMethod()")
         }
     }
 }
